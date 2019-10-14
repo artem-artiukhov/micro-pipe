16a7a9af861704170e31027bb6cd77c162fe87d2
aartiukhov <artem.artiukhov@chromeriver.com>
2019-10-14 09:33:18 +0300

    added handling of temporary files

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+import os
+import shutil
 
 from downloader import Downloader
 from reader import Reader
 from db_pusher import DBPusher
 from db_getter import Reporter
-from helpers import logger
+from helpers import logger, DEST_FOLDER, CSV_FOLDER, db_name
 
 logger.info('*********************')
 logger.info('* PIPE STARTED HERE *')
@@ -38,3 +40,12 @@ db_getter.disconnect()
 logger.info('*******************')
 logger.info('* PIPE ENDED HERE *')
 logger.info('*******************')
+
+# removing all files pipe worked with
+logger.info('Removing temporary folder with ndjson files...')
+shutil.rmtree(DEST_FOLDER)
+logger.info('Removing temporary folder with csv files...')
+shutil.rmtree(CSV_FOLDER)
+logger.info('Removing local database storage...')
+os.remove(db_name)
+logger.info('Everything is cleaned up. Enjoy your day!')
